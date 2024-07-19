c59dae5385fc3a6ac67c9f68510a336ca99c0e57
galbadrakh23 <galbadrak223@gmail.com>
2024-07-19 12:08:26 +0800

    July 19

--- components/hero.jsx
+++ components/hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
         <button className="button-big">GET started a project</button>
       </div>
       <div className="image">
-        <img src="/images/zurag.png" alt="zurag" />
+        <img src="/images/Zurag.png" alt="zurag" />
       </div>
     </div>
   );
